fd5ec6393737e85e020e0ba513228aa59ad780fa
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-08-24 15:49:30 -0400

    ori

---
+++ run-KAIROS-temporal.sh
@@ -0,0 +1,12 @@
+# export NVIDIA_VISIBLE_DEVICES=-1
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+export GRB_LICENSE_FILE=/shared/ccgadmin/app/gurobi/gurobi_client.lic
+export CUDA_VISIBLE_DEVICES=-1;
+#while [ 1 -eq 1 ]
+#do
+#    python server.py 1 4024
+#done
+do
+    python server.py dickens.seas.upenn.edu 4024
+done
